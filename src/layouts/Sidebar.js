169a524555b0c956b4e56afe33d8d570edfc615c
SI-Kim <48465526+SI-Kim@users.noreply.github.com>
2021-06-07 17:38:32 +0900

    set layout of side bar

--- src/layouts/Sidebar.js
+++ src/layouts/Sidebar.js
@@ -1,15 +1,36 @@
 import React from "react";
 import { Button } from "@material-ui/core";
 import { Link } from "react-router-dom";
+import { makeStyles } from "@material-ui/styles";
 
 const Sidebar = () => {
+  const classes = useStyle();
+
   return (
-    <div>
+    <div className={classes.sideBox}>
       <Link to="/counter">
-        <Button children="Counter" variant="contained" />
+        <Button
+          children="Counter"
+          variant="contained"
+          className={classes.btn}
+        />
       </Link>
+      <Button children="To do" variant="contained" className={classes.btn} />
     </div>
   );
 };
+
+const useStyle = makeStyles((theme) => ({
+  sideBox: {
+    width: "25vw",
+    height: "90vh",
+    backgroundColor: "grey",
+    textAlign: "center"
+  },
+  btn: {
+    margin: "8%",
+    width: "80%"
+  }
+}));
 
 export default Sidebar;
